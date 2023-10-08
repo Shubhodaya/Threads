e3c1a943e95ff4685a70976d0a5c1e3d03f05c67
Shubhodaya Sarkar <Shubhodayasarkar@gmail.com>
2023-10-08 14:11:47 +0530

    implemented

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,23 +1,23 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-//import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
- async function Page() {
+async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = {};
-  // if (userInfo?.onboarded) redirect("/"); 
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user?.id,
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user.imageUrl,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   };
 
   return (
@@ -30,10 +30,8 @@ import AccountProfile from "@/components/forms/AccountProfile";
       <section className='mt-9 bg-dark-2 p-10'>
         <AccountProfile user={userData} btnTitle='Continue' />
       </section>
-
     </main>
   );
- }
-
+}
 
 export default Page;
